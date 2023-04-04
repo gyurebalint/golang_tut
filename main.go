1c4b6e5cc01944866df1ca59dca4bac7dab491c2
gyurebalint <45357571+gyurebalint@users.noreply.github.com>
2023-04-05 00:09:13 +0200

    user defined types

--- main.go
+++ main.go
@@ -9,11 +9,43 @@ import (
 	"strings"
 )
 
-func main() {
-	excerciseToTestWhatYouLearned_1()
+type Tsp float64
+type TBs float64
+type ML float64
 
+func tspToML(tsp Tsp) ML {
+	return ML(tsp * 4.92)
 }
 
+func TBToML(tbs TBs) ML {
+	return ML(tbs * 14.79)
+}
+
+func (tsp Tsp) ToMLs() ML {
+	return ML(tsp * 4.92)
+}
+
+func (tbs TBs) ToMLs() ML {
+	return ML(tbs * 14.79)
+}
+
+func main() {
+	excerciseToTestWhatYouLearned_1()
+	definedTypes()
+
+}
+func definedTypes() {
+	ml1 := ML(Tsp(3) * 4.92)
+	fmt.Printf("3 tsp = %.2f ML\n", ml1)
+	ml2 := ML(TBs(3)) * 14.79
+	fmt.Printf("3 tbs = %.2f ML\n", ml2)
+
+	fmt.Printf("3tsp = %.2f mL\n", tspToML(3))
+	fmt.Printf("3tbs = %.2f mL\n", TBToML(3))
+
+	tsp1 := Tsp(3)
+	fmt.Printf("%.2f tsp = %.2f mL\n", tsp1, tsp1.ToMLs())
+}
 func excerciseToTestWhatYouLearned_1() {
 	reader := bufio.NewReader(os.Stdin)
 	fmt.Println("What is you age: ")
